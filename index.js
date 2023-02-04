c6165ea5c27326bf7812261235533a898a910829
Catalystix <dcode.green@gmail.com>
2023-02-04 08:56:53 -0500

    updated prompt questions to be more specific

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Manager = require('./lib/Manager');
 
 const newEmpdata = [];
 
-inquirer
+const response = await inquirer
     .promt([
         {
             type: 'input',
@@ -26,29 +26,74 @@ inquirer
             message: 'what is your id number?',
             name: 'id',
         },
+               {
+            type: 'list',
+            message: 'What is your role?',
+            name: 'role',
+            choices: ['Engineer', 'Intern', 'Manager'],
+        },
+       
+    ])
+
+if (response.role === 'Manager') {
+    const managerQ = await inquirer
+        .promt([
+            {
+                type: 'input',
+                message: 'what is your office number?',
+                name: 'onumber',
+            },
+        ])
+    const newManager = new Manager(
+        response.name,
+        response.id,
+        response.email,
+        managerQ.onumber,
+    );
+    newEmpdata.push(newManager);
+
+} else if (response.role === 'Engineer') {
+    const engineerQ = await inquirer
+    .promt([
         {
             type: 'input',
             message: 'what is your github username?',
             name: 'github',
-        },
+        },        
+    ])
+    const newEngineer = new Engineer (
+        response.name,
+        response.id,
+        response.email,
+        engineerQ.github
+    );
+    newEmpdata.push(newEngineer);
+
+} else if (response.role === 'Intern') {
+    const internQ = await inquirer
+    .promt ([
         {
             type: 'input',
             message: 'what school do you attend?',
             name: 'school',
         },
-        {
-            type: 'input',
-            message: 'what is your office number?',
-            name: 'onumber',
-        },
     ])
-    .then((Response) => {
-        writeToFile('index.html', response);
-        console.log('Thanks for Your information')
-        console.log('response list', response)
-    });
+    const newIntern = new Intern (
+        response.name,
+        response.id,
+        response.email,
+        internQ.school
+    );
+    newEmpdata.push(newIntern);
+}
 
-    function writeToFile(filename, response) {
-        fs.writeFile('index.html', genTeam(response), (err) =>
+    .then((Response) => {
+    writeToFile('index.html', response);
+    console.log('Thanks for Your information')
+    console.log('response list', response)
+});
+
+function writeToFile(filename, response) {
+    fs.writeFile('index.html', genTeam(response), (err) =>
         err ? console.error(err) : console.log('add log'))
-    }
+}
---
+++ src/GenTeam.js
@@ -0,0 +1,14 @@
+function renderInput (response) {
+    return  `    
+
+    ${response.name}
+    ${response.id}
+    ${response.email}
+    ${response.github}
+    ${response.onumber}
+    ${response.school}
+    
+    `
+};
+
+module.exports = GenTeam;
