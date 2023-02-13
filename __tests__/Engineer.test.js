bdac5febc92f430beaa62072abf7c5a1cd90baae
Catalystix <dcode.green@gmail.com>
2023-02-13 14:55:55 -0500

    updated tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,40 +1,41 @@
 const { default: expect } = require('expect');
-const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
 const name = "Drew";
 const empId = 33;
 const email = "come@me.com";
 const gitHub = 'catalystix@';
 
 test('initiate', () => {
-    const Emp = new Employee();
-    expect(Emp.getRole()).toBe('Employee');
+    const Emp = new Engineer();
+    expect(Emp.getRole()).toBe('Engineer');
 });
 
 test('will be the name', () => {
     
-    const Emp = new Employee(name);
+    const Emp = new Engineer(name);
     expect(Emp.name).toBe(name);
     expect(Emp.getName()).toBe(name);
 });
 
 test('will be the ID', () => {
    
-    const Emp = new Employee(name, empId);
+    const Emp = new Engineer(name, empId);
     expect(Emp.id).toBe(empId);
     expect(Emp.getId()).toBe(empId);
 });
 
 test('will be the email', () => {
     
-    const Emp = new Employee(name, empId, email);
+    const Emp = new Engineer(name, empId, email);
     expect(Emp.email).toBe(email);
     expect(Emp.getEmail()).toBe(email);
 });
 
 test('will be github', () => {
-    const Emp = new Employee(name, empId, email, gitHub);
+    const Emp = new Engineer(name, empId, email, gitHub);
     expect(Emp.gitHub).toBe(gitHub);
     expect(Emp.getGitHub()).toBe(gitHub);
-
+console.log('this is github', Emp.gitHub);
+console.log('this is github function', Emp.getGitHub);
 
 });
